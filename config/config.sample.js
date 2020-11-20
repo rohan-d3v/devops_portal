248526a6c446c13cf9baff9b2cb31540585ec2b1
Rohan Krishna <rohan@pagefy.org>
2020-11-20 20:27:27 +0530

    Config Sample Update

---
+++ config/config.sample.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'dbURI' : 'mongodb://localhost:27017/devops_portal?authSource=admin',
+    
+    'mailerConfig': {
+        host: 'smtp.gmail.com', port: 465, secure: true,
+        auth: {
+            user: "",
+            pass: ""
+        }
+    },
+    'passportClientSecret': 'LongIslandIcedTeaOrSomeOtherRandomKeygen',
+};
